cd8f038df5640963e7dd2b946644e79bdb7bfcc5
Unknown <mackan240@gmail.com>
2018-04-05 10:02:44 +0200

    Basic skeleton of server and embedded

---
+++ PunchManiaEmbedded/PunchManiaEmbedded.ino
@@ -0,0 +1,19 @@
+//ADXL345
+#include <SparkFun_ADXL345.h>
+//Ethernet
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
