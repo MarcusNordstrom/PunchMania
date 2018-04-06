a14b08033671d319c7741ba5228a6ae2799452f3
Unknown <abrondin@gmail.com>
2018-04-06 19:42:32 +0200

    lyckas inte ändra i UI från client....

--- PunchManiaPC_Client/src/client/Client.java
+++ PunchManiaPC_Client/src/client/Client.java
@@ -1,35 +1,60 @@
 package client;
 
+import java.awt.Dimension;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
 public class Client implements Observer {
 	private Socket socket;
 	private DataInputStream dis; 
-	private int data;
 
+	private int data = 0;
+	private String test;
 
-	public Client(String ip, int port) {
-		try {
-			socket = new Socket(ip, port);
-			dis = new DataInputStream(socket.getInputStream());
+	private UIHighScore uiHS;
 
-
-		} catch (IOException e) {}
+	public Client(UIHighScore uiHS) {
+		this.uiHS = uiHS;
 	}
+
+	//	public Client(String ip, int port) {
+	//		try {
+	//			socket = new Socket(ip, port);
+	//			dis = new DataInputStream(socket.getInputStream());
+	//
+	//		
+	//		} catch (IOException e) {}
+	//	}
 
 
 	public void update(Observable o, Object arg) {
-		try {
-			data = dis.readInt();
-			System.out.println(data);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//		try {
+		//			data = dis.readInt();
+		//			System.out.println(data);
+		//		} catch (IOException e) {
+		//			e.printStackTrace();
+		//		}
 
 	}
 
+
+	public void updateUI() {
+		test = JOptionPane.showInputDialog("skriv");
+		uiHS.updateHighScore(test);
+	}
+
+
+	public static void main(String[] args) {
+		UIHighScore uiHS = new UIHighScore();
+		Client cli = new Client(uiHS);
+		cli.updateUI();
+
+	}
 }
--- PunchManiaPC_Client/src/client/UIHighScore.java
+++ PunchManiaPC_Client/src/client/UIHighScore.java
@@ -1,21 +1,32 @@
 package client;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class UIHighScore extends JPanel {
+public class UIHighScore extends JPanel implements ActionListener {
 	private JLabel lblHighScore = new JLabel("High Score");
-	private JTextArea taNames = new JTextArea("test \ntest \ntest \ntest ");
+	private JTextArea taNames = new JTextArea();
+	private JButton btn = new JButton("Update");
+
+	private String text = "apa";
 
 	public UIHighScore() {
 		setLayout(new BorderLayout());
 		add(lblHighScore, BorderLayout.NORTH);
 		add(taNames, BorderLayout.CENTER);
+		add(btn, BorderLayout.SOUTH);
 		taNames.setEditable(false);
+		btn.addActionListener(this);
 
 	}
 
-	public static void main(String[] args) {
+	public void updateHighScore(String info) {
+		text = info; 							// HUR KOMMER JAG ÅT "INFO" FRÅN EN ANNAN METOD?? 
+		taNames.setText(info);
+
 		JFrame frame = new JFrame();
 		frame = new JFrame("PUNCH MANIA - HIGH SCORE");
 		frame.setResizable(false);
@@ -25,5 +36,27 @@ public class UIHighScore extends JPanel {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+	public static void main(String[] args) {
+		//		JFrame frame = new JFrame();
+		//		frame = new JFrame("PUNCH MANIA - HIGH SCORE");
+		//		frame.setResizable(false);
+		//		frame.setPreferredSize(new Dimension(500,600));
+		//		frame.add(new UIHighScore());
+		//		frame.pack();
+		//		frame.setLocationRelativeTo(null);
+		//		frame.setVisible(true);
+		//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btn) {
+			taNames.setText(text);
+			System.out.println(text);
+		}
+
 	}
 }
