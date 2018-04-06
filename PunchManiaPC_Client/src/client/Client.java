8ec632e7bcdd519c38a33f55d6630d24b6ac01f9
Unknown <abrondin@gmail.com>
2018-04-06 16:39:56 +0200

    Påbörjat klienten pyttelite och kladd som inte funkar.

--- PunchManiaPC_Client/src/client/Client.java
+++ PunchManiaPC_Client/src/client/Client.java
@@ -1,5 +1,35 @@
 package client;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Client implements Observer {
+	private Socket socket;
+	private DataInputStream dis; 
+	private int data;
+
+
+	public Client(String ip, int port) {
+		try {
+			socket = new Socket(ip, port);
+			dis = new DataInputStream(socket.getInputStream());
+
+
+		} catch (IOException e) {}
+	}
+
+
+	public void update(Observable o, Object arg) {
+		try {
+			data = dis.readInt();
+			System.out.println(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
