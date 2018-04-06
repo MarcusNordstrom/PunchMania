f7b90546ea93f3bec2b1685158ecd63557797841
Unknown <imjakeeyy@gmail.com>
2018-04-06 12:50:02 +0200

    Simpel server, tar emot UTF

--- PunchManiaServer/src/server/Server.java
+++ PunchManiaServer/src/server/Server.java
@@ -1,5 +1,67 @@
 package server;
 
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+
+public class Server{
+	private ServerSocket serverSocket;
+	private Connection connection = new Connection();
+
+	public Server(int port) {
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		connection.start();
+
+	}
+
+	private class ClientHandler implements Runnable{
+		private DataOutputStream dos;
+		private DataInputStream dis;
+		private String value;
+
+		public ClientHandler(Socket socket) {
+			try {
+				dis = new DataInputStream(socket.getInputStream());
+				dos = new DataOutputStream(socket.getOutputStream());
+
+			} catch(IOException e) {}
+		}
+
+		public void run() {
+
+			try {
+				value = dis.readUTF();
+				System.out.println(value);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public class Connection extends Thread {
+
+		public void run() {
+			System.out.println("Server running, listening to port: " + serverSocket.getLocalPort() + "\n");
+			while (true) {
+				try {
+					Socket socket = serverSocket.accept();
+					ClientHandler ch = new ClientHandler(socket);
+					ch.run();
+
+				} catch (IOException e) {
+					System.err.println(e);
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Server server = new Server(12345);
+	}
 }
