91bb787f5b0182729d6afb3ca2e9df1667c43f01
Unknown <abrondin@gmail.com>
2018-04-11 16:04:15 +0200

    test client, vet inte hur man gör

--- PunchManiaPC_Client/src/client/Client.java
+++ PunchManiaPC_Client/src/client/Client.java
@@ -1,43 +1,44 @@
 package client;
 
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Observable;
 import java.util.Observer;
-
 import javax.swing.JOptionPane;
+
+
 
 
 public class Client implements Observer {
 	private Socket socket;
 	private DataInputStream dis; 
-
 	private int data = 0;
 	private String test;
-
 	private UIHighScore uiHS;
+
 
 	public Client(UIHighScore uiHS) {
 		this.uiHS = uiHS;
 	}
 
-	//	public Client(String ip, int port) {
-	//		try {
-	//			socket = new Socket(ip, port);
-	//			dis = new DataInputStream(socket.getInputStream());
-	//
-	//		
-	//		} catch (IOException e) {}
-	//	}
+	public Client(String ip, int port) {
+		try {
+			socket = new Socket(ip, port);
+			dis = new DataInputStream(socket.getInputStream());
+
+
+		} catch (IOException e) {}
+	}
 
 
 	public void update(Observable o, Object arg) {
-		//		try {
-		//			data = dis.readInt();
-		//			System.out.println(data);
-		//		} catch (IOException e) {
-		//			e.printStackTrace();
-		//		}
+		try {
+			data = dis.readInt();
+			System.out.println(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -49,10 +50,12 @@ public class Client implements Observer {
 	}
 
 
-	public static void main(String[] args) {
-		UIHighScore uiHS = new UIHighScore();
-		Client cli = new Client(uiHS);
-		cli.updateUI();
+	public static void main(String[] args) throws IOException {
+		Client client = null;
+		client = new Client("IPADRESS",12346);
 
+			UIHighScore uiHS = new UIHighScore();
+			Client cli = new Client(uiHS);
+			cli.updateUI();
 	}
 }
