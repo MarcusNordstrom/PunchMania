1e5cf56ed92761a20817e9838c7c0586e1cec68b
Unknown <mackan240@gmail.com>
2018-04-11 17:24:36 +0200

    Embedded dev, not much changed

--- PunchManiaEmbedded/PunchManiaEmbedded.ino
+++ PunchManiaEmbedded/PunchManiaEmbedded.ino
@@ -9,9 +9,9 @@
 
 //CONFIG
 //Ethernet
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF }; //Mac address
-IPAddress server(74,125,232,128); //Ip address for server                                                     CHANGE LATER
-IPAddress ip(192, 168, 0, 101); //This device
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //Mac address
+IPAddress server(192, 168, 1, 35); //Ip address for server                                                     CHANGE LATER
+IPAddress ip(192, 168, 1, 101); //This device
 EthernetClient client;
 //ADXL I2C
 ADXL345 adxl = ADXL345();
@@ -25,37 +25,44 @@ void setup() {
   //DEBUG - REMOVE LATER
   Serial.begin(9600);
   Serial.println();
-
   //ADXL startup
   adxl.powerOn(); //Power on accelerometer
   adxl.setRangeSetting(16); //Sets sensitivty to 16G
   //Ethernet startup
-  if (Ethernet.begin(mac) == 0) {
+  /*if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
-  }
+  }*/
+  Ethernet.begin(mac, ip);  //EXPERIMENTAL REPLACE LINE WITH COMMENT ABOVE
   delay(1000);
-  if (client.connect(server, 80)) {                                                                         //CHANGE PORT
+  Serial.println(Ethernet.localIP());
+  Serial.println("connecting...");
+  int status = 100;
+  if (status = client.connect(server, 12345)) {                                                                         //CHANGE PORT
     Serial.println("connected");
   } else {
     Serial.println("connection failed");
   }
+  Serial.println("Status: ");
+  Serial.println(status);
 }
 String storage = "";
 void loop() {
   // axis variables
-  int x = INT_MAX;
-  int y = INT_MAX;
-  int z = INT_MAX;
+  int x, y, z;
   adxl.readAccel(&x, &y, &z); //Reads the accelerometer values and stores them in the axis variables
   //if any axis exceeds the hitvalue (both positive and negative) set boolean to true
   if(x > hitValue || x < -hitValue || y > hitValue || y < -hitValue || z > hitValue || z < -hitValue) {
     hit_detected = true;
-    Serial.print("H");
+    Serial.println("h:");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println(z);
   }
   //if an hit has been detected save values for 1000 cycles then set the boolean and counter to false resp. 0
   if(hit_detected) {
     if(counter < 1000) {
-      if(!(x == INT_MAX || y == INT_MAX || z == INT_MAX)) {    //Extra safety for null values
+      //if(!(x == INT_MAX || y == INT_MAX || z == INT_MAX)) {    //Extra safety for null values
         counter++;
         /*Serial.print(x);
         Serial.print(", ");
@@ -68,12 +75,12 @@ void loop() {
         storage += ",";
         storage += String(z);
         storage += ";";
-      }
+      //}
     } else {
       counter = 0;
       hit_detected = false;
       client.print(storage);
-      //Serial.println(storage);
+      Serial.println(storage);
       storage = "";
     }
   } /*else {       EXPERIMENTAL UNTESTED UNIT FOR DUPLEX COMUNICATION
