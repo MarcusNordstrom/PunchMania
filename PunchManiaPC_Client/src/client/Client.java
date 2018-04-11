71074c200f6a78ddf2d0b47967425a01c79ac723
Unknown <mackan240@gmail.com>
2018-04-11 17:25:41 +0200

    Auto stash before merge of "master" and "origin/master"

--- PunchManiaPC_Client/src/client/Client.java
+++ PunchManiaPC_Client/src/client/Client.java
@@ -53,7 +53,7 @@ public class Client {
 
 	public static void main(String[] args) throws IOException {
 		Client client = null;
-		client = new Client("IPADRESS",12346);
+		client = new Client("127.0.0.1",12346);
 
 		UIHighScore uiHS = new UIHighScore();
 		Client cli = new Client(uiHS);
