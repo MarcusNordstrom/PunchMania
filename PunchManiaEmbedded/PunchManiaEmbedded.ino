cb6671c115f609d24490a34fac1c6be29bb57b1c
Petoovee <Petomatick@gmail.com>
2018-04-12 19:42:13 +0200

    Added self-calibration for IS
    The IS should now remember every 50th cycle for up to 300 cycles, using the average of those to calculate an offset to use for the corresponding axis.

--- PunchManiaEmbedded/PunchManiaEmbedded.ino
+++ PunchManiaEmbedded/PunchManiaEmbedded.ino
@@ -21,6 +21,13 @@ boolean hit_detected = false;
 int counter = 0;
 //String for storage of all values
 String storage = "";
+//Calibration variables
+int calibrationCounter = 0;
+int calibrationXStorage[6] = {0,0,0,0,0,0};
+int calibrationYStorage[6] = {0,0,0,0,0,0};
+int calibrationZStorage[6] = {0,0,0,0,0,0};
+
+
 void setup() {
   //DEBUG - REMOVE LATER
   Serial.begin(9600);
@@ -51,6 +58,57 @@ void loop() {
   int x, y, z;
   adxl.readAccel(&x, &y, &z); //Reads the accelerometer values and stores them in the axis variables
   //if any axis exceeds the hitvalue (both positive and negative) set boolean to true
+  
+  calibrationXOffset = 0;
+  calibrationYOffset = 0;
+  calibrationZOffset = 0;
+  calibrationCounter++;
+  switch(calibrationCounter)) {
+	  case 50:
+	  calibrationXStorage[0] = x;
+	  calibrationYStorage[0] = y;
+	  calibrationZStorage[0] = z;
+	  end;
+	  case 100:
+	  calibrationXStorage[1] = x;
+	  calibrationYStorage[1] = y;
+	  calibrationZStorage[1] = z;
+	  end;
+	  case 150:
+	  calibrationXStorage[2] = x;
+	  calibrationYStorage[2] = y;
+	  calibrationZStorage[2] = z;
+	  end;
+	  case 200:
+	  calibrationXStorage[3] = x;
+	  calibrationYStorage[3] = y;
+	  calibrationZStorage[3] = z;
+	  end;
+	  case 250:
+	  calibrationXStorage[4] = x;
+	  calibrationYStorage[4] = y;
+	  calibrationZStorage[4] = z;
+	  end;
+	  case 300:
+	  calibrationXStorage[5] = x;
+	  calibrationYStorage[5] = y;
+	  calibrationZStorage[5] = z;
+	  calibrationCounter = 0
+	  end;
+  }
+  
+  for(loop = 0; loop < 6; loop++) {
+	  calibrationXOffset += calibrationXStorage[loop];
+	  calibrationYOffset += calibrationYStorage[loop];
+	  calibrationZOffset += calibrationZStorage[loop];
+  }
+  calibrationXOffset = calibrationXOffset/6;
+  calibrationYOffset = calibrationYOffset/6;
+  calibrationZOffset = calibrationZOffset/6;
+  x -= calibrationXOffset;
+  y -= calibrationYOffset;
+  z -= calibrationZOffset;
+  
   if(x > hitValue || x < -hitValue || y > hitValue || y < -hitValue || z > hitValue || z < -hitValue) {
     hit_detected = true;
     Serial.println("h:");
