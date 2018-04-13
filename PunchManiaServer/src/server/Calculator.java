61e197ee72de2b9246562e4b2b2ba7beb820b203
Jake <imjakeeyy@gmail.com>
2018-04-13 11:17:21 +0200

    done

--- PunchManiaServer/src/server/Calculator.java
+++ PunchManiaServer/src/server/Calculator.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class Calculator {
@@ -11,6 +12,7 @@ public class Calculator {
 	int Z;
 	private String section;
 	private ArrayList<String> splittedString = new ArrayList<String>();
+	
 	private ArrayList<Integer> x = new ArrayList<Integer>();
 	private ArrayList<Integer> y = new ArrayList<Integer>();
 	private ArrayList<Integer> z = new ArrayList<Integer>();
@@ -36,27 +38,24 @@ public class Calculator {
 
 		for(int i=0; i < splittedString.size(); i++) {
 			section = splittedString.get(i);
-	
-			for(int o=0; o <section.length(); o++) {
-				int last_colon = 0;
-				
-//				if(col == section.charAt(o)) {
-//					String subx = section.substring(last_colon, o);
-//					System.out.println(subx + "XXX");
-//					last_colon = o;
-//				}
-//				if(col == section.charAt(o)) {
-//					String suby = section.substring(last_colon, ',');
-//					System.out.println(suby + "YYY");
-//					last_colon = o;
-//				}
-//				if(col == section.charAt(o)) {
-//					String subz = section.substring(last_colon+1, section.length());
-//					System.out.println(subz + "ZZZ");
-//					last_colon = o;
-//					
-//				}
+			String[] dev = section.split(",");
+			
+			for(int x1 = 0; x1 < dev.length; x1+=3) {
+				String xx1 = dev[x1];
+				x.add(Integer.parseInt(xx1));
+			}
+			for(int y1= 1; y1 <dev.length; y1+=3) {
+				String yy1 = dev[y1];
+				y.add(Integer.parseInt(yy1));
+			}
+			for(int z1= 2; z1 <dev.length; z1+=3) {
+				String zz1 = dev[z1];
+				z.add(Integer.parseInt(zz1));
 			}
 		}
+		System.out.println(x.toString());
+		System.out.println(y.toString());
+		System.out.println(z.toString());
 	}
+
 }
