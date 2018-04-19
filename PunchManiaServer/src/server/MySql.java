bcf0eddcc91fa026d7127759f12b5fa45c524936
Jake <imjakeeyy@gmail.com>
2018-04-19 22:09:04 +0200

    Auto stash before merge of "master" and "origin/master"
    SQL started on

---
+++ PunchManiaServer/src/server/MySql.java
@@ -0,0 +1,42 @@
+package server;
+
+import java.sql.*;
+
+import common.UserList;
+
+public class MySql {
+	private String URL = "jdbc:mysql://localhost:8889/HSList";
+	private String Password = "root";
+	private String UserName = "root";
+
+	private Connection myConn = null;;
+
+	public MySql() {
+		try {
+
+			myConn = DriverManager.getConnection(URL, UserName, Password);
+			System.out.println("SQL Connected");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setMySql(String name, int score) {
+		try {
+			Statement Stmt = myConn.createStatement();
+			String update = "INSERT INTO HSList(Name, Score) VALUES ('" + name + "', " + score + ")";
+			Stmt.execute(update);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new MySql();
+	}
+
+}
+
+
