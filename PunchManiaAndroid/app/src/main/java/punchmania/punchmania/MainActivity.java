2f7a845277e8397e60e6492ef1a8bab4ceb6d6d7
Petoovee <Petomatick@gmail.com>
2018-04-20 13:27:23 +0200

    Removed switch

--- PunchManiaAndroid/app/src/main/java/punchmania/punchmania/MainActivity.java
+++ PunchManiaAndroid/app/src/main/java/punchmania/punchmania/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add to queue", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
