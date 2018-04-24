79a6590dc645c7675bf1365e31373f2a580f2c5b
Marcus Nordström <mackan240@gmail.com>
2018-04-24 21:55:51 +0200

    Added queue functionality

--- PunchManiaWebb/PunchMania/login.php
+++ PunchManiaWebb/PunchMania/login.php
@@ -9,17 +9,23 @@
 <body>
 <?php  
 if (isset($_GET["reg"])) {
+	if (isset($_GET["error"])) {
+		echo "Någon har det användarnamnet!";
+	}
 	echo "Registrera dig";
-	echo '<form action="regdb.php" method="POST">
-	<input type="text" name="uname" pattern=".{3,15}">
-	<input type="password" name="pw">
+	echo '<form action="dbmanager.php?type=reg" method="POST">
+	<input type="text" name="uname" pattern=".{3,15}" required>
+	<input type="password" name="pw" required>
 	<input type="submit">
 </form>';
 } else {
+	if (isset($_GET['error'])) {
+		echo "Fel lösenord!";
+	}
 	echo "Logga in";
-	echo '<form action="logindb.php" method="POST">
-	<input type="text" name="uname" pattern=".{3,15}">
-	<input type="password" name="pw">
+	echo '<form action="dbmanager.php?type=login" method="POST">
+	<input type="text" name="uname" pattern=".{3,15}" required>
+	<input type="password" name="pw" required>
 	<input type="submit">
 </form>';
 }
