94c1137a82f6505ab18899fd1c05945860a17d4e
Petoovee <Petomatick@gmail.com>
2018-04-27 11:29:41 +0200

    Yesterdays stuff

--- PunchManiaAndroid/app/src/main/java/punchmania/punchmania/HighScoreDetails.java
+++ PunchManiaAndroid/app/src/main/java/punchmania/punchmania/HighScoreDetails.java
@@ -2,10 +2,11 @@ package punchmania.punchmania;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 public class HighScoreDetails extends AppCompatActivity {
-    private static final String TAG = "HighScoreListActivity";
+    private static final String TAG = "HighScoreDetails";
     private ListView mListView;
 
     @Override
@@ -13,6 +14,6 @@ public class HighScoreDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.highscorelist_layout);
         mListView = (ListView) findViewById(R.id.highScoreListView);
-        getParent().
+        MainActivity.getHighScoreDetails(HighScoreListActivity.getClickedItemId())
     }
 }
