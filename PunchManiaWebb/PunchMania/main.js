21f36b70b2a49e71f5344ecbf5aa27b1404c1257
Marcus Nordström <mackan240@gmail.com>
2018-05-03 15:08:50 +0200

    web fixes, serviceworker and much more

--- PunchManiaWebb/PunchMania/main.js
+++ PunchManiaWebb/PunchMania/main.js
@@ -1,1 +1,11 @@
-//TODO: Make an active list update when ppl hit
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('http://ddwap.mah.se/ah7115/PunchMania/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ PunchManiaWebb/PunchMania/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(evt) {
+	console.log('The service worker is being installed.');
+});
+
+self.addEventListener('fetch', function(evt) {
+
+});
